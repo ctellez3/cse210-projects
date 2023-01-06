c2f93c5c66f9a1f65ddb57b3268504e956af7b72
Christian <c.tcadavid@gmail.com>
2023-01-06 14:17:47 -0500

    Names and message changed.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        String FirstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        String LastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}.");
     }
 }
