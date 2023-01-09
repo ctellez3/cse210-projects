dc954da5c3447af4d8195316203916614e434ea1
Christian <c.tcadavid@gmail.com>
2023-01-09 12:50:29 -0500

    Program created.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int random = randomGenerator.Next(1, 100); 
+        int guess = -1; 
+
+    while (random != guess)
+    {
+        Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+
+        if (guess > random)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess < random)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+    }
     }
 }
