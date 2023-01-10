d20bafdbd281c4922d6116be85f8181a72bc5328
Christian <c.tcadavid@gmail.com>
2023-01-10 06:18:10 -0500

    Program created.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int newNumber = -1; 
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (newNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string answer = Console.ReadLine(); 
+            newNumber = int.Parse(answer);
+        
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber);
+            }
+        }
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;    
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        double avg = Queryable.Average(numbers.AsQueryable());
+        Console.WriteLine($"The average is: {avg}");
+
+        int maxNumber = numbers.Max();
+        Console.WriteLine($"The largest number is: {maxNumber}");
     }
 }
