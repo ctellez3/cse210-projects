c10617e7247169840db76f4581fd89f2bbc85ffa
Christian <c.tcadavid@gmail.com>
2023-01-11 05:54:24 -0500

    Program created.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            double squaredNumber = Math.Pow(number, 2); 
+            Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+        }
     }
 }
